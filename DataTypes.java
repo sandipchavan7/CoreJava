20d692e7fbcb4d25194bbbaa3ebd60a1045b378f
Sandip Chavan <90947012+sandipchavan7@users.noreply.github.com>
2023-03-31 11:40:13 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,17 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        // primitive DTs - int,char,float,double,byte,boolean,short,long
+
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.SIZE);
+        System.out.println(Integer.SIZE / 8);
+
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.SIZE);
+        System.out.println(Double.SIZE / 8);
+
+    }
+
+}
